0e78e286ca9dac31ec9678ee119786afb6561e80
Edivaldo Andrade <edivaldo_andrade10@hotmail.com>
2021-10-13 02:24:41 +0100

    Creating a component for links in the navigation bar

---
+++ components/AppLinks.vue
@@ -0,0 +1,45 @@
+<template>
+    <ul class="nav-list">
+        <li class="nav-item"><nuxt-link to="/">Clube</nuxt-link></li>
+        <li class="nav-item"><nuxt-link to="/">Loja</nuxt-link></li>
+        <li class="nav-item"><nuxt-link to="/">Produtores</nuxt-link></li>
+        <li class="nav-item"><nuxt-link to="/">Ofertas</nuxt-link></li>
+        <li class="nav-item"><nuxt-link to="/">Eventos</nuxt-link></li>
+    </ul>
+</template>
+
+<style scoped>
+
+    .nav-list {
+        display: flex;
+        list-style: none;
+        padding: 0;
+        margin: 0 20px;
+    }
+
+    .nav-item {
+        margin: 0 15px;
+    }
+
+    .nav-item a {
+        text-decoration: none;
+        color: #666666;
+    }
+
+    .nav-item a:hover,
+    .nav-item a:active{
+        color: #3a3a3a;
+    }
+
+    @media (max-width: 767px) {
+        .nav-list {
+            /* display: block; */
+            margin: 0;
+        }
+        .nav-item{
+            margin-top: 16px;
+            margin-bottom: 16px;
+        }
+    }
+
+</style>
