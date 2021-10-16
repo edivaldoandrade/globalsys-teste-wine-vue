28dd05048ba1551f863429b5ed2dc9d3c6711835
Edivaldo Andrade <edivaldo_andrade10@hotmail.com>
2021-10-16 13:58:45 +0100

    Integrating cart features

--- components/TheSideNav.vue
+++ components/TheSideNav.vue
@@ -1,366 +1,338 @@
 <template>
-    <div class="sidenav-container">
+  <div class="sidenav-container">
+    <div v-if="isSidebar" class="backdrop" @click="hideSidebar"></div>
 
-        <div v-if="isSidebar" class="backdrop" @click="hideSidebar"></div>
+    <transition name="slide-side">
+      <div v-if="isSidebar" class="sidenav">
+        <div class="sidenav-header">
+          <span @click="hideSidebar">&#128473;</span>
+          <span>WineBox ({{ totalItemsCart }})</span>
+        </div>
+        <div class="sidenav-content">
+          <div class="over-y">
+            <div class="product" v-for="(item, index) in products" :key="index">
+              <div class="product-item">
+                <img class="product-image" :src="item.image" />
 
-        <transition name="slide-side">
-            <div v-if="isSidebar" class="sidenav">
-                <div class="sidenav-header">
-                    <span @click="hideSidebar">&#128473;</span>
-                    <span>WineBox (5)</span>
-                </div>
-                <!-- <app-links></app-links> -->
-                <div class="sidenav-content">
-                    <div class="over-y">
-                        <div class="product" v-for="(item, index) in products" :key="index">
-                            <div class="product-item">
-
-                                <img class="product-image" :src="item.image" />
-                                
-                                <div class="prod">
-                                    <div class="product-info">
-                                        <h5 class="product-title">{{ item.name }}</h5>
-                                        <div>
-                                            <div class="product-quantity">
-                                                <span>1</span>
-                                                <div class="qty_sub qty_button">
-                                                    <span>-</span>
-                                                </div>
-                                                <div class="qty_add qty_button">
-                                                    <span>+</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="product-price">
-                                        <div class="remove-product">&#128473;</div>
-                                        <p class="price"><span>{{ 32 | toCurrency }}</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr v-if="index !=Object.keys(products).length - 1" />
+                <div class="prod">
+                  <div class="product-info">
+                    <h5 class="product-title">{{ item.name }}</h5>
+                    <div>
+                      <div class="product-quantity">
+                        <span>{{ item.quantity }}</span>
+                        <div
+                          class="qty_sub qty_button"
+                          @click="subFromCart(item)"
+                        >
+                          <span>-</span>
                         </div>
-                    </div>
-
-                    <div class="sidenav-footer">
-                        <div class="subtotal">
-                            <span class="subtotal-text">subtotal</span>
-                            <span class="price">{{ 233 | toCurrency }}</span>
+                        <div
+                          class="qty_add qty_button"
+                          @click="addToCart(item)"
+                        >
+                          <span>+</span>
                         </div>
-                        <button class="btn btn-order">Finalizar pedido</button>
+                      </div>
                     </div>
+                  </div>
+
+                  <div class="product-price">
+                    <div
+                      class="remove-product"
+                      @click.prevent="removeFromCart(item)"
+                    >
+                      &#128473;
+                    </div>
+                    <p class="price">
+                      <span>{{ item.priceStock | toCurrency }}</span>
+                    </p>
+                  </div>
                 </div>
+              </div>
+              <hr v-if="index != Object.keys(products).length - 1" />
             </div>
-        </transition>
+          </div>
 
-
-    </div>
+          <div class="sidenav-footer">
+            <div class="subtotal">
+              <span class="subtotal-text">subtotal</span>
+              <span class="price">{{ cartSubtotal | toCurrency }}</span>
+            </div>
+            <button class="btn btn-order">Finalizar pedido</button>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
-
-    export default {
-
-        data() {
-            return {
-                products: [
-                    {
-    "available": true,
-    "image": "https://www.wine.com.br/cdn-cgi/image/q=99,f=auto,h=176/assets-images/produtos/19728-01.png",
-    "name": "Oxford Landing Cabernet Shiraz 2016",
-    "priceMember": 47.9,
-    "pricePromotional": 56.35,
-    "priceStock": 120.9
+export default{
+  computed: {
+    isSidebar() {
+      return this.$store.getters["nav/toggleSidebar"];
     },
-                    {
-    "available": true,
-    "image": "https://www.wine.com.br/cdn-cgi/image/q=99,f=auto,h=176/assets-images/produtos/19728-01.png",
-    "name": "Oxford Landing Cabernet Shiraz 2016",
-    "priceMember": 47.9,
-    "pricePromotional": 56.35,
-    "priceStock": 120.9
+    products() {
+      return this.$store.getters["cart/cart"];
+    },
+    totalItemsCart() {
+      return this.$store.getters["cart/cartAmount"];
+    },
+    cartSubtotal() {
+      return this.$store.getters["cart/cartSubtotal"];
+    },
   },
-  {
-    "available": true,
-    "image": "https://www.wine.com.br/cdn-cgi/image/q=99,f=auto,h=176/assets-images/produtos/23315-01.png",
-    "name": "Partridge Flying Malbec 2019",
-    "priceMember": 34.9,
-    "pricePromotional": 41.06,
-    "priceStock": 59.9
+
+  methods: {
+    hideSidebar() {
+      this.$store.dispatch("nav/toggleSidebar");
+    },
+    addToCart(item) {
+      this.$store.commit("cart/addToCart", item);
+      this.$store.commit("cart/saveCart");
+    },
+    subFromCart(item) {
+      this.$store.commit("cart/subFromCart", item);
+      this.$store.commit("cart/saveCart");
+    },
+    removeFromCart(item) {
+      this.$store.commit("cart/removeFromCart", item);
+      this.$store.commit("cart/saveCart");
+    },
   },
-  {
-    "available": true,
-    "image": "https://www.wine.com.br/cdn-cgi/image/q=99,f=auto,h=176/assets-images/produtos/19728-01.png",
-    "name": "Oxford Landing Cabernet Shiraz 2016",
-    "priceMember": 47.9,
-    "pricePromotional": 56.35,
-    "priceStock": 120.9
-  },
-  {
-    "available": true,
-    "image": "https://www.wine.com.br/cdn-cgi/image/q=99,f=auto,h=176/assets-images/produtos/19728-01.png",
-    "name": "Oxford Landing Cabernet Shiraz 2016",
-    "priceMember": 47.9,
-    "pricePromotional": 56.35,
-    "priceStock": 120.9
-  }
-]
-            }
-        },
-
-        methods: {
-            
-            hideSidebar() {
-                this.$store.dispatch('nav/toggleSidebar')
-            }
-        },
-
-        computed: {
-
-            isSidebar() {
-                return this.$store.getters['nav/toggleSidebar']
-            }
-
-        },
-
-
-    }
-
+};
 </script>
 
 
 <style scoped>
+.over-y {
+  overflow-y: auto;
+  height: calc(100vh - 185px);
+}
 
-    .over-y {
-        overflow-y: auto;
-        height: calc(100vh - 185px);
-    }
+.over-y::-webkit-scrollbar {
+  display: none;
+}
 
-    .over-y::-webkit-scrollbar {
-      display: none;
-    }
+.sidenav-footer {
+  box-shadow: 0px -2px 5px rgb(0 0 0 / 5%);
+  bottom: 0px;
+  position: absolute;
+  width: -webkit-fill-available;
+}
 
-    .sidenav-footer {
-        box-shadow: 0px -2px 5px rgb(0 0 0 / 5%);
-        bottom: 0px;
-        position: absolute;
-        width: -webkit-fill-available;
-    }
-    
-    .subtotal {
-        display: flex;
-        justify-content: space-between;
-        padding-bottom: 14px;
-    }
+.subtotal {
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 14px;
+}
 
-    .subtotal-text {
-        font-style: normal;
-        font-weight: 600;
-        font-size: 10px;
-        line-height: 12px;
+.subtotal-text {
+  font-style: normal;
+  font-weight: 600;
+  font-size: 10px;
+  line-height: 12px;
 
-        text-align: right;
-        text-transform: uppercase;
+  text-align: right;
+  text-transform: uppercase;
 
-        color: #666666;
-    }
-    
-    .price {
-        font-family: Lato;
-        font-style: normal;
-        font-weight: bold;
-        font-size: 14px;
-        line-height: 17px;
+  color: #666666;
+}
 
-        text-align: right;
-        margin-top: 0;
+.price {
+  font-family: Lato;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 17px;
 
-        display: flex;
-        align-items: center;
+  text-align: right;
+  margin-top: 0;
 
-        color: #B6116E;
-    }
+  display: flex;
+  align-items: center;
 
-    .sidenav-header span {
-        margin: 0 4px; 
-        font-style: normal;
-        font-weight: normal;
-        font-size: 20px;
-        line-height: 16px;
-        letter-spacing: -0.833333px;
-    }
+  color: #b6116e;
+}
 
-    .btn {
-        width: 100%;
-        height: 40px;
-        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15);
-        border: none;
-        border-radius: 4px;
-        
-        font-size: 14px;
-        line-height: 17px;
-        color: #FFFFFF;
-        
-        cursor: pointer;
-    }
+.sidenav-header span {
+  margin: 0 4px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 16px;
+  letter-spacing: -0.833333px;
+}
 
-    .btn-order {
-        background: #7FBC44;
-    }
+.btn {
+  width: 100%;
+  height: 40px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15);
+  border: none;
+  border-radius: 4px;
 
-    .backdrop {
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.7);
-        z-index: 1000;
-        position: fixed;
-        top: 0;
-        right: 0;
-    }
+  font-size: 14px;
+  line-height: 17px;
+  color: #ffffff;
 
-    .slide-side-enter-active,
-    .slide-side-leave-active {
-        transition: all 0.3s ease-out;
-    }
-    .slide-side-enter,
-    .slide-side-leave-to {
-        transform: translateX(+100%);
-    }
+  cursor: pointer;
+}
 
-    @media (max-width: 767px) {
-        .sidenav {
-            width: 100%;
-        }
-    }
-    
-    .product-area {
-    margin: 38px -15px 30px -15px;
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-wrap: wrap;
-    flex-wrap: wrap;
-  }
+.btn-order {
+  background: #7fbc44;
+}
 
-  .product {
-    position: relative;
+.backdrop {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+  right: 0;
+}
+
+.slide-side-enter-active,
+.slide-side-leave-active {
+  transition: all 0.3s ease-out;
+}
+.slide-side-enter,
+.slide-side-leave-to {
+  transform: translateX(+100%);
+}
+
+@media (max-width: 767px) {
+  .sidenav {
     width: 100%;
-    max-height: 165px;
-    padding: 10px;
-    box-sizing: border-box;
   }
+}
 
-  .product-item {
-    display: flex;
-    flex-direction: row;
-  }
+.product-area {
+  margin: 38px -15px 30px -15px;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+}
 
-  .product-image {
-    display: block;
-    object-fit: cover;
-    align-self: center;
-    max-height: 120px;
-  }
+.product {
+  position: relative;
+  width: 100%;
+  max-height: 165px;
+  padding: 10px;
+  box-sizing: border-box;
+}
 
-  .prod {
-    width: 100%;
-      display: flex;
-      justify-content: space-between;
-  }
-  
-  .product-info {
-        align-content: space-between;
-        display: grid;
-    }
+.product-item {
+  display: flex;
+  flex-direction: row;
+}
 
-  .product-title {
-      margin: 0;
-  }
-  
-  .product-quantity {
-      position: relative;
-    width: 68px;
-    height: 30px;
-    border: solid 2px #969696;
-    border-radius: 3px;
+.product-image {
+  display: block;
+  object-fit: cover;
+  align-self: center;
+  max-height: 120px;
+}
 
-    text-align: center;
-  }
+.prod {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
 
-  .product-quantity > span {
-    line-height: 28px;
-    font-size: 16px;
-    font-weight: 300;
-    color: #4a4a4a;
-    text-align: center;
+.product-info {
+  align-content: space-between;
+  display: grid;
+}
+
+.product-title {
+  margin: 0;
+}
+
+.product-quantity {
+  position: relative;
+  width: 68px;
+  height: 30px;
+  border: solid 2px #969696;
+  border-radius: 3px;
+
+  text-align: center;
+}
+
+.product-quantity > span {
+  line-height: 28px;
+  font-size: 16px;
+  font-weight: 300;
+  color: #4a4a4a;
+  text-align: center;
 }
 
 .qty_button {
-    position: absolute;
-    top: 50%;
-    -webkit-transform: translateY(-50%);
-    -moz-transform: translateY(-50%);
-    -ms-transform: translateY(-50%);
-    -o-transform: translateY(-50%);
-    transform: translateY(-50%);
-    width: 22px;
-    height: 100%;
-    line-height: 28px;
-    cursor: pointer;
-    font-size: 16px;
-    color: #414141;
+  position: absolute;
+  top: 50%;
+  -webkit-transform: translateY(-50%);
+  -moz-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  -o-transform: translateY(-50%);
+  transform: translateY(-50%);
+  width: 22px;
+  height: 100%;
+  line-height: 28px;
+  cursor: pointer;
+  font-size: 16px;
+  color: #414141;
 }
 
 .qty_button span {
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 
 .qty_sub {
-    left: 0px;
+  left: 0px;
 }
 
 .qty_add {
-    right: 0px;
+  right: 0px;
 }
 
-    .product-price {
-        align-content: space-between;
-        display: grid;
-        justify-items: end;
-    }
+.product-price {
+  align-content: space-between;
+  display: grid;
+  justify-items: end;
+}
 
-    .product-price .price {
-        margin: 0;
-    }
+.product-price .price {
+  margin: 0;
+}
 
-  .remove-product {
-    width: 19px;
-    height: 19px;
-    border-radius: 50%;
-    border: 1px solid #979797;
+.remove-product {
+  width: 19px;
+  height: 19px;
+  border-radius: 50%;
+  border: 1px solid #979797;
 
-    display: block;
-    font-style: normal;
-    line-height: 20px;
-    font-size: 12px;
-    color: #888888;
-    text-align: center;
+  display: block;
+  font-style: normal;
+  line-height: 20px;
+  font-size: 12px;
+  color: #888888;
+  text-align: center;
 
-    -webkit-transform: translateY(1px);
-    -moz-transform: translateY(1px);
-    -ms-transform: translateY(1px);
-    -o-transform: translateY(1px);
-    transform: translateY(1px);
-    
-    cursor: pointer;
-  }
+  -webkit-transform: translateY(1px);
+  -moz-transform: translateY(1px);
+  -ms-transform: translateY(1px);
+  -o-transform: translateY(1px);
+  transform: translateY(1px);
 
-  .product hr {
-      margin-top: 20px;
-      margin-bottom: 20px;
-  }
+  cursor: pointer;
+}
 
+.product hr {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,308 +1,322 @@
 <template>
   <div class="content">
-
     <div class="banner-box">
-      <img class="banner-full" src="~/assets/images/banner-full.png" alt="banner">
-      <img class="banner-responsive" src="~/assets/images/banner-responsive.png" alt="banner">
+      <img
+        class="banner-full"
+        src="~/assets/images/banner-full.png"
+        alt="banner"
+      />
+      <img
+        class="banner-responsive"
+        src="~/assets/images/banner-responsive.png"
+        alt="banner"
+      />
     </div>
 
     <div class="product-area">
       <div class="prd" v-for="(product, index) in products" :key="index">
-          <article class="product-card">
+        <article class="product-card">
+          <img class="product-image" :src="product.image" />
 
-            <img class="product-image" :src="product.image" />
-            
-            <div class="article-details">
-              <div><h4 class="product-title">{{ product.name }}</h4>
-              <div v-if="product.available"  class="product-price">
-
-                <p class="product-author">{{ product.priceStock | toCurrency }}</p>
+          <div class="article-details">
+            <div>
+              <h4 class="product-title">{{ product.name }}</h4>
+              <div v-if="product.available" class="product-price">
+                <p class="product-author">
+                  {{ product.priceStock | toCurrency }}
+                </p>
 
                 <div class="member">
                   <div>
                     <p>Sócio</p>
                     <p>Wine</p>
                   </div>
-                  <p class="price"><span>{{ product.priceMember | toCurrency }}</span></p>
+                  <p class="price">
+                    <span>{{ product.priceMember | toCurrency }}</span>
+                  </p>
                 </div>
-                
-                <p class="price-responsive"> Não sócio {{ product.priceStock | toCurrency }}</p>
 
+                <p class="price-responsive">
+                  Não sócio {{ product.priceStock | toCurrency }}
+                </p>
               </div>
-              </div>
-              
-
-              <div>
-                <button v-if="product.available" class="btn btn-add f-1" @click="addToCart(index, 1)">Adicionar</button>
-                <button v-else class="btn btn-non-available f-1">Esgotado</button>
-              </div>
-
             </div>
-          </article>
-          <div>
-            <button v-if="product.available" class="btn btn-add-responsive f-1" @click="addToCart(index, 1)">Adicionar</button>
-            <button v-else class="btn btn-non-available-responsive f-1">Esgotado</button>
+
+            <div>
+              <button
+                v-if="product.available"
+                class="btn btn-add f-1"
+                @click="addToCart(product)"
+              >
+                Adicionar
+              </button>
+              <button v-else class="btn btn-non-available f-1">Esgotado</button>
+            </div>
           </div>
+        </article>
+        <div>
+          <button
+            v-if="product.available"
+            class="btn btn-add-responsive f-1"
+            @click="addToCart(product)"
+          >
+            Adicionar
+          </button>
+          <button v-else class="btn btn-non-available-responsive f-1">
+            Esgotado
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
-  export default {
-
-    methods: {
-      addToCart(item) {
-        this.$store.commit('addToCart', item);
-      }
+export default {
+  methods: {
+    addToCart(item) {
+      this.$store.commit("cart/addToCart", item);
+      this.$store.commit("cart/saveCart");
     },
-    async asyncData({ $http }) {
-      const products = await $http.$get('https://run.mocky.io/v3/908ec5b5-1e5a-4602-9008-47719f7c6759')
+  },
+  async asyncData({ $http }) {
+    const products = await $http.$get(
+      "https://run.mocky.io/v3/908ec5b5-1e5a-4602-9008-47719f7c6759"
+    );
 
-      return { products }
-    }
-  }
+    return { products };
+  },
+};
 </script>
 
 <style scoped>
-  
+/*  */
+.product-area {
+  margin: 38px -10px 30px -10px;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+}
 
-
-  /*  */
+@media (max-width: 991px) {
   .product-area {
-    margin: 38px -10px 30px -10px;
-    display: -ms-flexbox;
     display: flex;
-    -ms-flex-wrap: wrap;
-    flex-wrap: wrap;
+    flex-wrap: unset;
   }
-  
-  @media (max-width: 991px){
-    .product-area {
-      display: flex;
-      flex-wrap: unset;
-    }
-  }
+}
 
+.prd {
+  position: relative;
+  /* width: 100%; */
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+@media (min-width: 992px) {
   .prd {
-    position: relative;
-    /* width: 100%; */
-    padding: 10px;
-    box-sizing: border-box;
+    -ms-flex: 0 0 33.33%;
+    flex: 0 0 33.33%;
+    max-width: 33.33%;
   }
+}
 
-  @media (min-width: 992px){
-    .prd{
-      -ms-flex: 0 0 33.33%;
-      flex: 0 0 33.33%;
-      max-width: 33.33%;    
-    }
+.product-card {
+  display: flex;
+  flex-direction: row;
+  background: #fff;
+  box-shadow: 0 0.1875rem 1.5rem rgba(0, 0, 0, 0.2);
+  border-radius: 0.375rem;
+  padding: 8px;
+
+  max-height: 210px;
+}
+
+.product-image {
+  display: block;
+  object-fit: cover;
+  align-self: center;
+  max-height: 176px;
+}
+
+.article-details {
+  font-style: normal;
+  font-weight: bold;
+
+  display: grid;
+  align-content: space-between;
+}
+
+.product-title {
+  font-size: 14px;
+  line-height: 16px;
+
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.2px;
+  margin: 0 0 15px 0;
+
+  color: #1d1d1b;
+}
+
+.member {
+  font-size: 10px;
+  line-height: 10px;
+  text-transform: uppercase;
+  color: #1d1d1b;
+}
+
+.member .price {
+  margin-top: 0;
+  font-size: 12px;
+
+  display: flex;
+  align-items: center;
+
+  color: #b6116e;
+}
+
+.member .price span {
+  font-size: 14px;
+}
+
+.price-responsive {
+  display: none;
+}
+
+.product-author {
+  font-size: 12px;
+  line-height: 16px;
+  text-decoration-line: line-through;
+  color: #888888;
+  margin: 0;
+}
+
+.member p:first-child {
+  margin-bottom: 4px;
+}
+
+.btn {
+  width: 100%;
+  height: 40px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15);
+  border: none;
+  border-radius: 4px;
+
+  font-size: 14px;
+  line-height: 17px;
+  color: #ffffff;
+
+  cursor: pointer;
+}
+
+.btn-add,
+.btn-add-responsive {
+  background: #b6116e;
+}
+
+.btn-non-available,
+.btn-non-available-responsive {
+  background: #a0a0a0;
+}
+
+.btn-add-responsive,
+.btn-non-available-responsive {
+  display: none;
+  margin-top: 10px;
+}
+
+@supports (display: grid) {
+  #root {
+    grid-area: main;
+    align-self: center;
+    justify-self: center;
   }
 
   .product-card {
-    display: flex;
-    flex-direction: row;
-    background: #fff;
-    box-shadow: 0 0.1875rem 1.5rem rgba(0, 0, 0, 0.2);
-    border-radius: 0.375rem;
-    padding: 8px;
-    
-    max-height: 210px;
-  }
-
-  .product-image {
-    display: block;
-    object-fit: cover;
-    align-self: center;
-    max-height: 176px;
-  }
-
-  .article-details{
-    font-style: normal;
-    font-weight: bold;
-    
     display: grid;
-    align-content: space-between;
+    grid-template-columns: 1fr 2fr;
+    grid-template-rows: 1fr;
   }
 
-  .product-title {
-    font-size: 14px;
-    line-height: 16px;
-
-    display: flex;
-    align-items: center;
-    letter-spacing: -0.2px;
-    margin: 0 0 15px 0;
-
-    color: #1D1D1B;
-  }
-
-  .member {
-    font-size: 10px;
-    line-height: 10px;
-    text-transform: uppercase;
-    color: #1D1D1B;
-  }
-
-  .member .price {
-    margin-top: 0;
-    font-size: 12px;
-
-    display: flex;
-    align-items: center;
-
-    color: #B6116E;
-  }
-
-  .member .price span{
-    font-size: 14px;
-  }
-
-  .price-responsive {
-    display: none;
-  }
-
-  .product-author {
-    font-size: 12px;
-    line-height: 16px;
-    text-decoration-line: line-through;
-    color: #888888;
-    margin: 0;
-  }
-
-  .member p:first-child {
-    margin-bottom: 4px;
-  }
-
-  .btn {
-    width: 100%;
-    height: 40px;
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15);
-    border: none;
-    border-radius: 4px;
-    
-    font-size: 14px;
-    line-height: 17px;
-    color: #FFFFFF;
-    
-    cursor: pointer;
-  }
-
-  .btn-add,
-  .btn-add-responsive
-  {
-    background: #B6116E;
-  }
-
-  .btn-non-available,
-  .btn-non-available-responsive
-  {
-    background: #A0A0A0;
-  }
-
-  .btn-add-responsive,
-  .btn-non-available-responsive
-   {
-    display: none;
-    margin-top: 10px;
-  }
-
-  @supports (display: grid) {
-    
-    #root {
-      grid-area: main;
-      align-self: center;
-      justify-self: center;
+  @media (max-width: 767px) {
+    .banner-full {
+      display: none;
     }
-    
+
+    .banner-responsive {
+      display: block;
+    }
+    .product-area {
+      display: flex;
+      overflow-x: auto;
+      overflow-y: hidden;
+      width: 756px;
+      height: auto;
+      -ms-flex-wrap: unset;
+      flex-wrap: unset;
+      margin: 38px -10px 30px -10px;
+    }
+
+    .product-area::-webkit-scrollbar {
+      display: none;
+    }
     .product-card {
-      display: grid;
-      grid-template-columns: 1fr 2fr;
-      grid-template-rows: 1fr;
-    }
-    
-    @media (max-width: 767px) {
-      .banner-full{
-        display:none;
-      }
+      grid-template-rows: 12rem 1fr;
+      flex-wrap: wrap;
+      display: inline-table;
+      width: 244px;
+      min-height: 340px;
 
-      .banner-responsive{
-        display:block;
-      }
-      .product-area {
-        display: flex;
-        overflow-x: auto;
-        overflow-y: hidden;
-        width: 756px;
-        height: auto;
-        -ms-flex-wrap: unset;
-        flex-wrap: unset;
-        margin: 38px -10px 30px -10px;
-      }
-
-      .product-area::-webkit-scrollbar {
-        display: none;
-      }
-      .product-card {
-        grid-template-rows: 12rem 1fr;
-        flex-wrap: wrap;
-        display: inline-table;
-        width: 244px;    
-        min-height: 340px;
-        
-        text-align: center;
-        text-align: -webkit-center;
-      }
-
-      .product-title {
-        font-size: 16px;
-        display: block;
-      }
-
-      .article-details {
-        text-align: center;
-      }
-      
-      .btn-add-responsive,
-      .btn-non-available-responsive
-      {
-        display: block;
-      }
-
-      .price-responsive{
-        display: block;
-      }
-
-      .member {
-        width: 100%;
-        display: inline-flex;
-        place-content: center;
-      }
-
-      .member .price{
-        display: flex;
-        place-self: end;
-      }
-
-      .btn-add, 
-      .btn-non-available 
-      {
-        display: none;
-      }
-
-      .product-image {
-        display: block;
-        object-fit: cover;
-        align-self: center;
-      }
+      text-align: center;
+      text-align: -webkit-center;
     }
 
-    @media (max-width: 480px){
-      .product-area {
-        width: 465px;
-      }
+    .product-title {
+      font-size: 16px;
+      display: block;
+    }
+
+    .article-details {
+      text-align: center;
+    }
+
+    .btn-add-responsive,
+    .btn-non-available-responsive {
+      display: block;
+    }
+
+    .price-responsive {
+      display: block;
+    }
+
+    .member {
+      width: 100%;
+      display: inline-flex;
+      place-content: center;
+    }
+
+    .member .price {
+      display: flex;
+      place-self: end;
+    }
+
+    .btn-add,
+    .btn-non-available {
+      display: none;
+    }
+
+    .product-image {
+      display: block;
+      object-fit: cover;
+      align-self: center;
     }
   }
 
+  @media (max-width: 480px) {
+    .product-area {
+      width: 465px;
+    }
+  }
+}
 </style>
